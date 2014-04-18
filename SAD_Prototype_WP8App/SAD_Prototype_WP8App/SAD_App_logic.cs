e0a74c03be6ebafd77fb3bcfc942f67eede564ed
venkat raman pandey <venkatrpandey@yahoo.com>
2014-04-18 20:05:02 +0200

    done

---
+++ SAD_Prototype_WP8App/SAD_Prototype_WP8App/SAD_App_logic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAD_Prototype_WP8App
+{
+    class SAD_App_logic
+    {
+    }
+}
