e324ddb9fab69ea378f590be49e9ff672d5b38b3
venkat raman pandey <venkatrpandey@yahoo.com>
2014-04-24 17:33:30 +0200

    ok

--- SAD_Prototype_WP8App/SAD_Prototype_WP8App/Parent_List.xaml.cs
+++ SAD_Prototype_WP8App/SAD_Prototype_WP8App/Parent_List.xaml.cs
@@ -30,27 +30,27 @@ namespace SAD_Prototype_WP8App
 
         private void button1_Click(object sender, RoutedEventArgs e)
         {
-            if (textBox1.Text != "")
-            {
-                listBox1.Items.Add(textBox1.Text);
-                textBox1.Text = "";
-            }
+            //if (textBox1.Text != "")
+            //{
+            //    listBox1.Items.Add(textBox1.Text);
+            //    textBox1.Text = "";
+            //}
         }
 
         private void listBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (e.AddedItems.Count > 0)
-            {
-                MessageBox.Show(listBox1.SelectedItem.ToString() + " will be deleted");
-                listBox1.Items.RemoveAt(listBox1.Items.IndexOf(listBox1.SelectedItem));
-                ((ListBox)sender).SelectedIndex = -1;
-            }
+            //if (e.AddedItems.Count > 0)
+            //{
+            //    MessageBox.Show(listBox1.SelectedItem.ToString() + " will be deleted");
+            //    listBox1.Items.RemoveAt(listBox1.Items.IndexOf(listBox1.SelectedItem));
+            //    ((ListBox)sender).SelectedIndex = -1;
+            //}
         }
 
         private void Menu_Loaded(object sender, RoutedEventArgs e)
         {
-            ListBox listBox = (sender as ListBox);
-            listBox.SelectedIndex = -1;
+            //ListBox listBox = (sender as ListBox);
+            //listBox.SelectedIndex = -1;
         }
 
       
