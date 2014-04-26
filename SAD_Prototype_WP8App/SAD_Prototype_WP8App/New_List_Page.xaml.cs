19733a5f383b9d24a9f9c120f8b73f8c7765af95
venkat raman pandey <venkatrpandey@yahoo.com>
2014-04-26 16:03:53 +0200

    new list page added

---
+++ SAD_Prototype_WP8App/SAD_Prototype_WP8App/New_List_Page.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace SAD_Prototype_WP8App
+{
+    public partial class New_List_Page : PhoneApplicationPage
+    {
+        public New_List_Page()
+        {
+            InitializeComponent();
+        }
+
+        private void AppBarSave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AppBarReminder_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
