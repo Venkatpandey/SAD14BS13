f3dcc6a07f848f1f5f6f8f4b3bf12020c8d4580b
venkat raman pandey <venkatrpandey@yahoo.com>
2014-05-09 11:54:14 +0200

    Folder Arranged

---
+++ SAD_Prototype_WP8App/old_unsynced_project/SAD_Prototype_WP8App/List_Page.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+
+namespace SAD_Prototype_WP8App
+{
+    public partial class List_Page : PhoneApplicationPage
+    {
+        public List_Page()
+        {
+            InitializeComponent();
+        }
+
+        private void AppBarSave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AppBarReminder_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NewItem_tb_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            CheckBox item = new CheckBox();
+            
+            
+            if (e.Key == System.Windows.Input.Key.Enter)
+            {
+                if (NewItem_tb.Text != string.Empty)
+                {
+                    item.Content = NewItem_tb.Text;
+                    item.Checked += item_Checked;
+                    ItemsList.Items.Add(item);
+                    
+                }
+            }
+        }
+
+        void item_Checked(object sender, RoutedEventArgs e)
+        {
+            
+            
+
+            CheckBox checkedItem = sender as CheckBox;
+            
+            //checkedItem.Content = string.Empty;
+
+        }
+
+        private void CheckBox_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+
+        internal void loadPage()
+        {
+            
+        }
+    }
+}
